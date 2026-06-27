ff87337b06ef83353e6d230f5b2f29aa3a974b25
nicoalesi <nicoalesi.cs@gmail.com>
2026-06-27 13:22:47 +0200

    fix: remove microsoft support
    Remove microsoft support since it's not supported in other sections of the code.

--- include/omega-five/common.hpp
+++ include/omega-five/common.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#if defined(_MSC_VER)
-    #define ALWAYS_INLINE __forceinline
-#elif defined(__GNUC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
     #define ALWAYS_INLINE inline __attribute__((always_inline))
 #else
     #define ALWAYS_INLINE inline
